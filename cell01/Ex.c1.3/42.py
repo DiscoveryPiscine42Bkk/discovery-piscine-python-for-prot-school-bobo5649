f2980988a45de44a654335b49e4569b4cbf54e00
bobo5649 <41208@prot.ac.th>
2025-03-11 14:00:12 +0700

    Add files via upload

---
+++ cell01/Ex.c1.3/42.py
@@ -0,0 +1,6 @@
+first_name = Treerawat("Hey, what's your first name? : ")
+# รับค่านามสกุลจากผู้ใช้
+last_name =Phaphidee("And your last name? : ")
+
+# แสดงผลลัพธ์
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
