d95400460d6566366009c3ac16276bb65fead6dc
bobo5649 <41208@prot.ac.th>
2025-03-13 14:27:28 +0700

    Add files via upload

---
+++ cell05/EX.c5.10/parameter_matching.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    parameter = sys.argv[1]
+    user_input = input("What was the parameter? ")
+
+    if user_input == parameter:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+
+if __name__ == "__main__":
+    main()
