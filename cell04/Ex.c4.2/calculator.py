e34023786a13a65612c37192e4c6ad5182826726
bobo5649 <41208@prot.ac.th>
2025-03-13 10:38:11 +0700

    Add files via upload

---
+++ cell04/Ex.c4.2/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# รับอินพุตตัวเลขสองตัวจากผู้ใช้
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+# แสดงผลลัพธ์ของการดำเนินการทางคณิตศาสตร์
+print("Thank you!")
+print(num1, "+", num2, "=", num1 + num2)
+print(num1, "-", num2, "=", num1 - num2)
+print(num1, "/", num2, "=", num1 / num2)
+print(num1, "*", num2, "=", num1 * num2)
