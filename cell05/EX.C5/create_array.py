dcdc3823e6ab7594fd036e1f24d764e0deeda2b4
bobo5649 <41208@prot.ac.th>
2025-03-13 10:55:45 +0700

    Add files via upload

---
+++ cell05/EX.C5/create_array.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def create_array():
+  """สร้างอาร์เรย์ของตัวเลขและแสดงผลบนหน้าจอ"""
+  my_array = [2, 8, 9, 48, 8, 22, -12, 2]
+  print(my_array)
+
+if __name__ == "__main__":
+  create_array()
